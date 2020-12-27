7b1de73b8f9d575b047ac4abd00fe3d04dce4ebb
hayashiny <75630500+hayashiny@users.noreply.github.com>
2020-12-27 14:09:31 -0500

    Delete iframe.js

--- docs/iframe.js
+++
@@ -1,9 +0,0 @@
-var iframe = document.getElementById("iframe");
-
-function setURL(url){
-    document.getElementById('iframe').src = url;
-}
-
-iframe.onload = function(){
-    iframe.style.height = iframe.contentWindow.document.body.scrollHeight + 'px';
-}
