391602b12d8b81117443610bb26d3da7ebbca069
hayashiny <75630500+hayashiny@users.noreply.github.com>
2021-01-06 16:26:54 -0500

    Update javascript.js

--- docs/javascript.js
+++ docs/javascript.js
@@ -121,7 +121,7 @@ iframe.addEventListener("load",function(){
                         return parseInt(bHeight)+100;
                         })();
         iframe.style.height = adjustedHeight + "px";
-        bodyRef.style.height = 100 + adjustedHeight + "px";
+        document.getElementsByTagName('body').height  =  bodyRef.scrollHeight + adjustedHeight + "px";
     }else{
         iframe.style.height = adjustedHeight + "px";
     }
