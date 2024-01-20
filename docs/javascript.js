8a6d3a53cb1b39cfe3f89b21efe6749c69d8dc3d
BuildTools <vlin0812@gmail.com>
2024-01-20 10:10:58 -0500

    fixed ui issues with title not disappearing

--- docs/javascript.js
+++ docs/javascript.js
@@ -41,4 +41,93 @@ function slide(e){
     })
 }
 window.addEventListener('scroll', debounce(slide));
+window.addEventListener('scroll', scrollFunc());
 
+function scrollFunc(){
+    var titleClass = document.getElementById('titleClass');
+    var navigationClass = document.getElementById("navigation");
+
+    if(window.screen.width >= 320 && window.screen.width <= 812){
+        if(window.scrollY > 100){
+            titleClass.style.opacity = "0";
+            titleClass.style.transform = "translate(-50%,200%)";
+        }else{
+            titleClass.style.transform = "translate(-50%,-50%)";
+        titleClass.style.opacity = "1";
+        }
+    }else if(window.scrollY > 500){
+        titleClass.style.opacity = "0";
+        titleClass.style.transform = "translate(-50%,200%)";
+    }else{
+        titleClass.style.transform = "translate(-50%,-50%)";
+        titleClass.style.opacity = "1";
+    }
+    
+    if(window.scrollY >= 1000){
+        if(window.screen.width >= 1300){
+            navigationClass.style.position = "fixed";
+            navigationClass.style.top = "0px"
+            navigationClass.style.margin = "18px 0px 0px"
+        }else{
+            navigationClass.style.position = "absolute";
+            navigationClass.style.top = "auto"
+            navigationClass.style.margin = "18px 0px 0px"
+        }
+    }else{
+        navigationClass.style.position = "absolute";
+        navigationClass.style.top = "auto";
+        navigationClass.style.marginTop = "18px";
+    }
+
+    //console.log(window.scrollY);
+}
+
+var height = document.getElementById("navigation").offsetHeight;
+var width = document.getElementById("navigation").offsetWidth;
+function navOpen(){
+    var elemt = document.getElementById("navigation");
+    var elemtIcon = document.getElementById("navigationIcon");
+    
+
+    if(parseInt(elemt.style.height) != height){
+        elemt.style.height = height + "px";
+        elemt.style.width = width + "px";
+        elemtIcon.style.transform = "rotate(90deg)";
+    }else{
+        elemt.style.height = "70px";    
+        elemt.style.width = "60px";
+        elemtIcon.style.transform = "rotate(0deg)";
+    } 
+    
+    //console.log(elemt.offsetHeight);
+    //console.log(height);
+}
+
+const iframe = document.getElementById('iframe');
+const loadText = document.getElementById('loadText');
+
+function setURL(url){
+    document.getElementById('iframe').src = url;
+    loadText.style.display = "";
+}
+
+iframe.addEventListener("load",function(){
+    loadText.style.display = "none";
+    var adjustedHeight = iframe.contentWindow.document.body.scrollHeight + 50;
+    if(window.screen.width < 1000){
+        var adjustedHeight = (function(){
+                        var bHeight = localStorage.getItem("bHeight");    
+                        console.log("bHeightGetter: " + bHeight);
+                    
+                        return parseInt(bHeight)+100;
+                        })();
+        iframe.style.height = adjustedHeight + "px";
+        document.getElementsByTagName('body').height  =  bodyRef.scrollHeight + adjustedHeight + "px";
+    }else{
+        iframe.style.height = adjustedHeight + "px";
+    }
+
+    console.log("Body: " + bodyRef.scrollHeight);
+    console.log("AdjustHeight: " + adjustedHeight);
+    console.log("iFrameHeight: " + iframe.style.height);
+})
